107745a0f93a462cf2eac2f706b41df637296dbb
Aleksandr Kiriushin <aleksandr.kiriushin@gatech.global>
2020-11-22 20:05:51 +0200

    Remove dotenv, add constant.js

--- frontend/billogram-test/src/App.js
+++ frontend/billogram-test/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from 'react';
+import {API} from './constants';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <p>This is an incredibly complex react app!</p>
+        <p>Server: {API}</p>
       </Fragment>
     );
   }
